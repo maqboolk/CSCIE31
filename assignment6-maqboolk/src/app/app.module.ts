e3392e5f6a0906e6d52df69e790f4da8478eb318
maqboolk <maqbool_kamthewala@fas.harvard.edu>
2021-05-02 13:42:37 -0400

    Assignment 6 midway

--- assignment6-maqboolk/src/app/app.module.ts
+++ assignment6-maqboolk/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { MenuComponent } from './menu/menu.component';
+import { MenuItemComponent } from './menu-item/menu-item.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent,
+    MenuItemComponent
   ],
   imports: [
     BrowserModule
---
+++ assignment6-maqboolk/src/app/mock-data.ts
@@ -0,0 +1,26 @@
+export const MOCKDATA = [
+    {
+        name: 'Chicken Schnitzel',
+        price: 11.99,
+        ingredients: 'Chickeen chicken chicken',
+        available: true
+    },
+    {
+        name: 'Drumsticks and String Beans',
+        price: 12.99,
+        ingredients: 'Chicken, string beans',
+        available: false
+    },
+    {
+        name: 'Salt and Pepper Steak',
+        price: 13.99,
+        ingredients: 'Beef Steak',
+        available: true
+    },
+    {
+        name: 'Steak Enchilada Bowls with Sweet Potato Rice',
+        price: 14.99,
+        ingredients: 'Steak , Sweet Potato, Rice',
+        available: true
+    }
+];
